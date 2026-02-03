d02c95e2d8e2f2e37087a4e002c7f128fe22e1db
James Ashford <ashfordj@rocketmail.com>
2026-02-03 15:46:59 +0100

    Sparser and prettier!

--- src/symba_alice_2026/basic_barricelli.py
+++ src/symba_alice_2026/basic_barricelli.py
@@ -2,9 +2,9 @@ from collections import Counter
 import matplotlib.pylab as plt
 import numpy as np
 
-SIZE = 256
-TIMESTEPS = 100
-MAX_VAL = 10
+SIZE = 1024
+TIMESTEPS = 200
+MAX_VAL = 3
 SEED = 1701
 
 # Basic baricelli copying
@@ -13,6 +13,7 @@ grid = np.zeros((TIMESTEPS, SIZE)).astype(int)
 
 # Initialise with random
 grid[0, :] = rng.integers(-MAX_VAL, MAX_VAL + 1, size=(SIZE))
+grid[0, rng.choice(np.arange(SIZE), size=SIZE // 2, replace=False)] = 0
 
 # Loop
 for step in range(1, TIMESTEPS):
@@ -26,5 +27,6 @@ print(Counter(grid[-1, :]))
 # View the final state!
 fig, ax = plt.subplots(1, 1)
 ax.imshow(grid / MAX_VAL, aspect="auto", interpolation="none", cmap="Blues")
+plt.axis("off")
 plt.savefig("out.png", bbox_inches="tight", transparent=True, dpi=200)
 plt.show()
